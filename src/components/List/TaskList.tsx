8aad7ac0d8b6fc2b8204c15ec47230126256a2ac
Vitalik0422 <vitaliy19980422@gmail.com>
2025-01-19 19:06:05 +0200

    liteChange

--- src/components/List/TaskList.tsx
+++ src/components/List/TaskList.tsx
@@ -2,9 +2,9 @@ import React, { useState } from 'react'
 import taskList from '../../store/taskList.json'
 
 export default function TaskList() {
-    const [isActive, setIsActive] = useState(true)
-    const onActive = () => {
-        setIsActive(!isActive)
+    const [visibleArticleId, setVisibleArticleId] = useState<string | null>(null)
+    const handleToggle  = (id:string) => {
+        setVisibleArticleId((prev) => (prev === id ? null : id));
     }
     return (
         <div className='taskList'>
@@ -17,9 +17,9 @@ export default function TaskList() {
                     </div>
                     <div className='taskData'>
                         <div className='taskTitle'>{item.title}
-                            <button type='button' onClick={onActive}>Active</button>
+                            <button type='button' onClick={() => handleToggle(item.id)}>Active</button>
                         </div>
-                        <div className={isActive ? 'taskText':'taskText active'}>{item.text}</div>
+                        <div className={visibleArticleId===item.id  ? 'taskText active':'taskText'}>{item.text}</div>
                     </div>
                 </div>
             ))}
